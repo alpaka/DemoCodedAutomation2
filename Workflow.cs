1e8fbb9c8e8a3203294f2a87e4b6501ebd7724df
Stefan Reutter <alpaka@gmail.com>
2023-11-03 10:47:25 +0100

    initial commit

---
+++ Workflow.cs
@@ -0,0 +1,40 @@
+using CodedAutomation2.ObjectRepository;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.Testing;
+using UiPath.Testing.Activities.TestData;
+using UiPath.Testing.Activities.TestDataQueues.Enums;
+using UiPath.Testing.Enums;
+using UiPath.UIAutomationNext.API.Contracts;
+using UiPath.UIAutomationNext.API.Models;
+using UiPath.UIAutomationNext.Enums;
+
+namespace CodedAutomation2
+{
+    public class Workflow : CodedWorkflow
+    {
+        [Workflow]
+        public int Execute(int n)
+        {
+                if (n <= 1)
+                    return n;
+        
+                int a = 0, b = 1, c = 0;
+        
+                for (int i = 2; i <= n; i++)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                }
+        
+                return c;
+                
+        }
+    }
+}
