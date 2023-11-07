709bfab981ef2c8cb5ffdbcce7c8c9127b5a729c
Stefan Reutter <alpaka@gmail.com>
2023-11-07 18:28:16 +0100

    some changes

--- Workflow.cs
+++ Workflow.cs
@@ -1,7 +1,7 @@
-using CodedAutomation2.ObjectRepository;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using CodedAutomation2.ObjectRepository;
 using UiPath.CodedWorkflows;
 using UiPath.Core;
 using UiPath.Core.Activities.Storage;
@@ -16,25 +16,24 @@ using UiPath.UIAutomationNext.Enums;
 
 namespace CodedAutomation2
 {
+    // Code Generation:
+    // add two numbers and log the result to console
     public class Workflow : CodedWorkflow
     {
         [Workflow]
         public int Execute(int n)
         {
-                if (n <= 1)
-                    return n;
-        
-                int a = 0, b = 1, c = 0;
-        
-                for (int i = 2; i <= n; i++)
-                {
-                    c = a + b;
-                    a = b;
-                    b = c;
-                }
-        
-                return c;
-                
+            if (n <= 1)
+                return n;
+            int a = 0, b = 1, c = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return c;
         }
     }
 }
